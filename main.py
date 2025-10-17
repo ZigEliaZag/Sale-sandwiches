32d73bb0ab3c79b03c05c8ae6ba3631bc75e8717
ZigEliaZag <eliakaliza50@icloud.com>
2025-10-17 14:28:30 +0200

    a function to get data string from user

--- main.py
+++ main.py
@@ -12,7 +12,23 @@ client = gspread.authorize(CREDS)
 sheet_id = "1pqXmgvomFqOWzbPYIQ98lN_r4N07CtYyY3JlzzieIVo"
 sheet = client.open_by_key(sheet_id)
 
-#data = sheet.sheet1.row_values(4)
+""" 
+-> this is code to check that API is working
+data = sheet.sheet1.row_values(4) 
 sales = sheet.worksheet("sales")
 data = sales.get_all_values()
 print(data) 
+"""
+
+def get_sales_date():
+    """
+    Get sales figures input from the user 
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here: ") #The value that user provide will be returned as string
+    print(f"The data provided is {data_str}")
+
+get_sales_date()
