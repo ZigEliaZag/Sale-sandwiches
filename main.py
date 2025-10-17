8cb3e56fa1f48c03da9ce8bc2b8c63d29f827d7a
ZigEliaZag <eliakaliza50@icloud.com>
2025-10-17 19:11:18 +0200

    add sales data to worksheet

--- main.py
+++ main.py
@@ -22,7 +22,10 @@ print(data)
 
 def get_sales_date():
     """
-    Get sales figures input from the user 
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -45,7 +48,7 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try: 
-        [int(value) for value in values]
+        [int(value) for value in values] #convert value to a integer
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values requireded, you provided {len(values)}"
@@ -56,4 +59,15 @@ def validate_data(values):
     
     return True
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = sheet.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
 data = get_sales_date()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
